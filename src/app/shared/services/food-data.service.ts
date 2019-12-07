4909f45b310d1d6ca45c8418fe45499dbf8e6daa
Yair Cosio <ycosio1310@gmail.com>
2019-12-07 12:29:06 -0600

    FoodDataService

---
+++ src/app/shared/services/food-data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Food } from '../models/food';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodDataService {
+
+  constructor(private _http: HttpClient) { }
+
+  public search(term): Observable<Food[]> {
+    return this._http.post(`https://api.nal.usda.gov/fdc/v1/search?api_key=${environment.foodApiKey}`, {
+      generalSearchInput: term
+    }).pipe(map(response => response['foods']));
+  }
+}
