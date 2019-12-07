8b755cfad70c2bba6fa72509d983dec24a8c0a41
Yair Cosio <ycosio1310@gmail.com>
2019-12-07 12:30:19 -0600

    HomeComponent: Default food list displayed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodDataService } from '../shared/services/food-data.service';
+import { take } from 'rxjs/operators';
+import { Food } from '../shared/models/food';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public foods: Food[] = [];
+
+  constructor(private _foodDataService: FoodDataService) {
+
+  }
+
+  ngOnInit() {
+    this.searchFood('meet');
+  }
+
+  searchFood(term) {
+    this._foodDataService.search(term)
+    .pipe(take(1))
+    .subscribe(foods => {
+      this.foods = foods;
+      console.log(this.foods);
+      
+    }, err => {
+      console.log(err);
+    });
+  }
 
 }
