8a2fc9eb944d5b9ccf42c9328d55eb448438e7ab
Yair Cosio <ycosio1310@gmail.com>
2019-12-07 13:00:20 -0600

    FoodModel: Keys updated

--- src/app/shared/models/food.ts
+++ src/app/shared/models/food.ts
@@ -1,11 +1,12 @@
 export interface Food {
     allHighlightFields: string;
-    brandOwner: string;
+    brandOwner?: string;
     dataType: string;
     description: string;
     fdcId: number;
     gtinUpc: string;
-    ingredients: string;
+    ingredients?: string;
     publishedDate: string;
     score: number;
+    foodCode?: string;
 }
